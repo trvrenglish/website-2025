8c0a172607c09c4efc273714f472597aa4469942
trvrenglish <trvrenglish@gmail.com>
2025-06-25 11:55:02 -0700

    rm prerender for testing

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		paths: {
-			base: '/website-2025'
+			base: process.env.NODE_ENV === 'production' ? '/website-2025' : ''
 		}
 	}
 };
