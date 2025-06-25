c8592e101b65e7d2c98d3267764f629fc06bd843
trvrenglish <trvrenglish@gmail.com>
2025-06-25 12:32:16 -0700

    changed base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -15,7 +17,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/website-2025' : ''
+			base: dev ? '' : '/website-2025'
 		}
 	}
 };
