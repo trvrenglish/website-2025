271a69697731a53761175fb3282f8f75ddafb490
trvrenglish <trvrenglish@gmail.com>
2025-06-25 12:45:56 -0700

    rm paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -14,11 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'app.html'
-		}),
-		paths: {
-			base: dev ? '' : '/website-2025'
-		}
+			fallback: 'index.html'
+		})
 	}
 };
 
