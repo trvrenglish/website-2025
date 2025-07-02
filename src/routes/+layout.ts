3c9ddd3651bf37b21893128516f64b644a7a7191
trvrenglish <trvrenglish@gmail.com>
2025-07-02 14:21:06 -0700

    removed env token for posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,11 +4,9 @@ export const prerender = true;
 import posthog from 'posthog-js';
 import { browser } from '$app/environment';
 
-const PUBLIC_POSTHOG_TOKEN = import.meta.env.VITE_POSTHOG_TOKEN;
-
 export const load = async () => {
 	if (browser) {
-		posthog.init(PUBLIC_POSTHOG_TOKEN, {
+		posthog.init('phc_IGpGy4rxHwyqPho5ATVa3MCiQIRZnC6F8QxuS9oo9GM', {
 			api_host: 'https://us.i.posthog.com',
 			defaults: '2025-05-24',
 			person_profiles: 'identified_only'
