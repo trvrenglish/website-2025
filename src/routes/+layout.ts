4c785717689a63f839c4a35f4afa6b63d6a4f8e0
trvrenglish <trvrenglish@gmail.com>
2025-07-02 14:14:26 -0700

    Posthog added

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,19 @@
 export const ssr = false;
 export const prerender = true;
+
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+
+const PUBLIC_POSTHOG_TOKEN = import.meta.env.VITE_POSTHOG_TOKEN;
+
+export const load = async () => {
+	if (browser) {
+		posthog.init(PUBLIC_POSTHOG_TOKEN, {
+			api_host: 'https://us.i.posthog.com',
+			defaults: '2025-05-24',
+			person_profiles: 'identified_only'
+		});
+	}
+
+	return;
+};
