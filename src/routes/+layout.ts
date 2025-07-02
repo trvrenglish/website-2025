b038b07a9bab65fa6adbd4f4186b82ca4e9585dc
trvrenglish <trvrenglish@gmail.com>
2025-07-02 14:24:14 -0700

    posthog moved to layout.js

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,2 @@
 export const ssr = false;
 export const prerender = true;
-
-import posthog from 'posthog-js';
-import { browser } from '$app/environment';
-
-export const load = async () => {
-	if (browser) {
-		posthog.init('phc_IGpGy4rxHwyqPho5ATVa3MCiQIRZnC6F8QxuS9oo9GM', {
-			api_host: 'https://us.i.posthog.com',
-			defaults: '2025-05-24',
-			person_profiles: 'identified_only'
-		});
-	}
-
-	return;
-};
